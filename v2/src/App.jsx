cfc2d0449c28c8d248022ec58a7224f89e40d2ad
Lukman Sk <skd1746289@gmail.com>
2023-10-15 21:12:33 +0530

    feat(client): add routes for video details and search results

--- v2/src/App.jsx
+++ v2/src/App.jsx
@@ -2,6 +2,8 @@ import { AppContext } from "./context/contextApi";
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Feed from "./components/Feed";
+import SearchResult from "../../v1/src/components/SearchResult";
+import VideoDetails from "./components/VideoDetails";
 const App = () => {
   return (
     <AppContext>
@@ -9,6 +11,8 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Feed />} />
+          <Route path="/searchResult/:searchQuery" element={<SearchResult />} />
+          <Route path="/video/:id" element={<VideoDetails />} />
         </Routes>
       </BrowserRouter>
     </AppContext>
